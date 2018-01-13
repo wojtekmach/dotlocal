7139629db68bb1e4a621767b5d606d3fb8cf21ee
Wojtek Mach <wojtek@wojtekmach.pl>
2018-01-13 23:15:20 +0100

    Register and resolve dnssd service

--- test/dotlocal_test.exs
+++ test/dotlocal_test.exs
@@ -13,12 +13,12 @@ end
 defmodule DotLocalTest do
   use ExUnit.Case
 
+  @tag :integration
   test "greets the world" do
-    assert {:error, _} = HTTPoison.get("http://localhost:4001/")
-
+    async_cmd!(~w(dns-sd -P hello-test _http._tcp local 4001 hello-test.local 192.168.0.200))
     start_hello()
 
-    assert HTTPoison.get!("http://localhost:4001/").body == "Hello world"
+    assert HTTPoison.get!("http://hello-test.local:4001/").body == "Hello world"
   end
 
   defp start_hello() do
@@ -27,5 +27,13 @@ defmodule DotLocalTest do
     ]
     opts = [strategy: :one_for_one, name: Hello.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp async_cmd!(args) do
+    Task.async(fn -> cmd!(args) end)
+  end
+
+  defp cmd!([cmd | args]) do
+    System.cmd(cmd, args, into: IO.stream(:stdio, :line))
   end
 end
