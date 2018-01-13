0443a6c71b4f8c5cdec322ffb9d1da02a290c8d1
Wojtek Mach <wojtek@wojtekmach.pl>
2018-01-13 23:55:40 +0100

    wip

--- lib/dotlocal.ex
+++ lib/dotlocal.ex
@@ -1,2 +1,24 @@
 defmodule DotLocal do
+  def register(name, port) do
+    # TODO: we call this async because dns-d blocks,
+    #       we should use a dnssd binding instead of cli
+    async_cmd!(~w(dns-sd -P #{name} _http._tcp local #{port} #{name}.local #{ip()}))
+  end
+
+  def proxy_cmd() do
+    ~s{echo "rdr pass inet proto tcp from any to any port 80 -> 127.0.0.1 port 8888" | sudo pfctl -ef -}
+  end
+
+  defp ip() do
+    {:ok, [{ip, _, _} | _]} = :inet.getif()
+    Enum.map_join(Tuple.to_list(ip), ".", &to_string/1)
+  end
+
+  defp async_cmd!(args) do
+    Task.async(fn -> cmd!(args) end)
+  end
+
+  defp cmd!([cmd | args]) do
+    System.cmd(cmd, args, into: IO.stream(:stdio, :line))
+  end
 end
--- test/dotlocal_test.exs
+++ test/dotlocal_test.exs
@@ -16,27 +16,20 @@ defmodule DotLocalTest do
   @tag :integration
   test "greets the world" do
     name = "hello-test2"
-    {:ok, [{ip, _, _} | _]} = :inet.getif()
-    ip = Enum.map_join(Tuple.to_list(ip), ".", &to_string/1)
-    async_cmd!(~w(dns-sd -P #{name} _http._tcp local 4001 #{name}.local #{ip}))
-    start_hello()
+    port = 4001
 
-    assert HTTPoison.get!("http://#{name}.local:4001/").body == "Hello world"
+    DotLocal.Proxy.start(8888)
+    start_hello(port)
+    DotLocal.register(name, port)
+
+    assert HTTPoison.get!("http://#{name}.local").body == "Hello world"
   end
 
-  defp start_hello() do
+  defp start_hello(port) do
     children = [
-      Plug.Adapters.Cowboy.child_spec(:http, Hello, [], [port: 4001])
+      Plug.Adapters.Cowboy.child_spec(:http, Hello, [], [port: port])
     ]
     opts = [strategy: :one_for_one, name: Hello.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  defp async_cmd!(args) do
-    Task.async(fn -> cmd!(args) end)
-  end
-
-  defp cmd!([cmd | args]) do
-    System.cmd(cmd, args, into: IO.stream(:stdio, :line))
   end
 end
